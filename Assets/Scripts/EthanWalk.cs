7bea48fd7113c758a874f6c6672ec241f16b6326
Joe Massey <joem29@hotmail.co.uk>
2016-07-03 11:19:00 +0100

    Forgot to include the build

---
+++ Assets/Scripts/EthanWalk.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EthanWalk : MonoBehaviour {
+
+	public float running = 0f;
+	public bool jump = false;
+
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (player.GetComponent<UnityStandardAssets.Characters.FirstPerson.RigidbodyFirstPersonController> ().movementSettings.isRunning) {
+			GetComponent<Animator> ().SetFloat ("Forward", 1f, 0.1f, Time.deltaTime);
+		} else {
+			GetComponent<Animator> ().SetFloat ("Forward", 0f, 0.1f, Time.deltaTime);
+		}
+
+		transform.position = player.transform.position + new Vector3(0,-1.2f,0);
+
+	}
+}
