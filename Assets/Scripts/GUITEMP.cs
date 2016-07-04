a7b48224701dc996e2ee31cd2702b9c6e290365d
Joe Massey <joem29@hotmail.co.uk>
2016-07-04 20:47:57 +0100

    Exit Shop Keyboard Button

--- Assets/Scripts/GUITEMP.cs
+++ Assets/Scripts/GUITEMP.cs
@@ -8,7 +8,7 @@ public class GUITEMP : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		ShopTracker = GameObject.FindGameObjectWithTag ("ShopController");
 	}
 	
 	// Update is called once per frame
--- Assets/Scripts/Shop/CurrencyCollect.cs
+++ Assets/Scripts/Shop/CurrencyCollect.cs
@@ -5,6 +5,11 @@ public class CurrencyCollect : MonoBehaviour {
 
 	public GameObject ShopTracker;
 
+	void Start() {
+		ShopTracker = GameObject.FindGameObjectWithTag ("ShopController");
+	}
+
+
 	void OnTriggerEnter(Collider Col) {
 		if (Col.tag == "Player") {
 			ShopTracker.GetComponent<ShopCurrencyTracker> ().playerCurrency++;
