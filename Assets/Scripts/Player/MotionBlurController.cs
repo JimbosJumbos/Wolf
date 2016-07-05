f4fece1c438907fb8b68c21ccbffcbaf4942ce33
Joe Massey <joem29@hotmail.co.uk>
2016-07-05 15:54:36 +0100

    Added Fog

---
+++ Assets/Scripts/Player/MotionBlurController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MotionBlurController : MonoBehaviour {
+
+	public GameObject player;
+	public bool active = false;
+
+	// Update is called once per frame
+	void Update () {
+
+		if (active == false) {
+			
+			if (player.GetComponent<UnityStandardAssets.Characters.FirstPerson.RigidbodyFirstPersonController> ().keyChange) {
+				active = true;
+				GetComponent<UnityStandardAssets.ImageEffects.MotionBlur> ().enabled = true;
+			} 
+		} else {
+			if (!player.GetComponent<UnityStandardAssets.Characters.FirstPerson.RigidbodyFirstPersonController> ().keyChange) {
+				active = false;;
+				GetComponent<UnityStandardAssets.ImageEffects.MotionBlur> ().enabled = false;
+			} 
+		}
+	
+	}
+}
